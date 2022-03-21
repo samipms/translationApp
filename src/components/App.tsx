f1a98390e72c3f0ab7ae8ab11701e35e71d98f50
Ip Sam <samip@microsoft.com>
2022-03-21 16:47:21 -0700

    updated translations app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,46 @@ import styles from './App.module.scss';
 // Initialize all app state/contexts here
 export const App = (): JSX.Element => {
     const [translation, setTranslation] = useState('');
+    const [fromLang, setFromLang] = useState('en');
+    const [toLang, setToLang] = useState('es');
 
     function handleTranslate(value) {
-        setTranslation(value);
+        const data = [{ text: value }];
+        const url =
+            'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=' +
+            fromLang +
+            '&to=' +
+            toLang;
+
+        console.log(url);
+        console.log(JSON.stringify(data));
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key': '28e1385cc5654626a1343662610d6424',
+                'Ocp-Apim-Subscription-Region': 'westus2',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', JSON.stringify(data[0]));
+                setTranslation(data[0].translations[0].text);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 
     return (
         <div>
             <h1>Translation App</h1>
 
-            <select name="sourceLang">
+            <select
+                name="fromLang"
+                onChange={(e) => setFromLang(e.target.value)}
+            >
                 <option value="en">English</option>
                 <option value="es">Spanish</option>
                 <option value="cn">Chinese</option>
@@ -30,9 +60,11 @@ export const App = (): JSX.Element => {
             />
             <br />
             <br></br>
-            <select name="targetLang">
+            <select name="toLang" onChange={(e) => setToLang(e.target.value)}>
                 <option value="en">English</option>
-                <option value="es">Spanish</option>
+                <option selected value="es">
+                    Spanish
+                </option>
                 <option value="cn">Chinese</option>
             </select>
             <br></br>
